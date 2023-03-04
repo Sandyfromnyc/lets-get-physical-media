a84d9c7094730ac044d8abb93950f6104e395ed1
EdASalazar <edasalazar123@gmail.com>
2023-03-03 21:53:24 -0800

    Add about and css

--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 # Create your views here.
 
-def home(request):
-    return render(request, 'home.html')
 
 def signup(request):
   error_message = ''
@@ -25,3 +26,10 @@ def signup(request):
   form = UserCreationForm()
   context = {'form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
+
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
