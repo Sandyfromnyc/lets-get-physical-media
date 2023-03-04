b517c814cb90ca4383a6f257774d57e865be42b3
EdASalazar <edasalazar123@gmail.com>
2023-03-04 09:20:09 -0800

    added movie to tape

--- main_app/models.py
+++ main_app/models.py
@@ -12,6 +12,7 @@ class Tape(models.Model):
   name = models.CharField(max_length=100)
   quantity = models.IntegerField()
   quality = models.CharField(max_length=100)
+  movies = models.ManyToManyField(Movie)
   user = models.ForeignKey(User, on_delete=models.CASCADE)
 
   def __str__(self):
