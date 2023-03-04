72d9b95962a5c4375be1ca97d5b9d576e9d60ae4
EdASalazar <edasalazar123@gmail.com>
2023-03-03 20:29:10 -0800

    Added LogIn and Log Out

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html')
+
