db79da6ec1be4aa61391f202e6f3023752d67aed
EdASalazar <edasalazar123@gmail.com>
2023-03-06 08:01:22 -0800

    working on tap creation process

--- main_app/views.py
+++ main_app/views.py
@@ -125,11 +125,13 @@ def search_movies(request):
   if request.method == 'POST':
     searched = request.POST['searched']
     movies = Movie.objects.filter(title__contains=searched)
-    # this works as the serach but need to figure out how to generate a tap from it
-    return render(request, 'search_movies.html', {'searched': searched, 'movies': movies})
+    # this works as the serach but we need to figure out how to generate a tap from it
+   # return render(request, 'search_movies.html', {'searched': searched, 'movies': movies})
     # return redirect(request, 'main_app/tape_form.html', {'searched': searched, 'movies': movies})
-    # return render(request, 'main_app/tape_form.html', {'searched': searched, 'movies': movies})
+    return render(request, 'main_app/tape_form.html', {'searched': searched, 'movies': movies})
   else:
+    # return render(request, 'search_movies.htm', {})
+    # return redirect(request, 'main_app/tape_form.html', {})
     return render(request, 'main_app/tape_form.html', {})
  
 
