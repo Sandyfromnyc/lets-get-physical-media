0d0f6b88069f4e2d96fcd8a597c61181af0f6f62
EdASalazar <edasalazar123@gmail.com>
2023-03-09 11:54:50 -0800

    Fixing the search

--- main_app/views.py
+++ main_app/views.py
@@ -119,11 +119,14 @@ def search_media(request):
 def search_movies(request):
   if request.method == 'POST':
     searched = request.POST['searched']
-    params = {'s': f'{searched}'}
-    response=requests.get('http://www.omdbapi.com/?apikey=acd8ae1a&', params=params).json()
-    search_response = response["Search"]
+    if searched != '':
+      params = {'s': f'{searched}'}
+      response=requests.get('http://www.omdbapi.com/?apikey=acd8ae1a&', params=params).json()
+      search_response = response["Search"]
+      return render(request, 'main_app/tape_form.html', {'searched': searched, 'search_response': search_response})
+    else:
+      return render(request, 'main_app/tape_form.html', {})
     
-    return render(request, 'main_app/tape_form.html', {'searched': searched, 'search_response': search_response})
   else:
     return render(request, 'main_app/tape_form.html', {})
   
